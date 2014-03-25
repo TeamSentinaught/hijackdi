983e778f359181c33d342ec7a6aeee411814243e
benoj <ben.j.flowers@gmail.com>
2014-03-25 10:36:24 -0700

    faking multiple dependencies

--- test/tests.js
+++ test/tests.js
@@ -1,4 +1,6 @@
 require('chai').should();
+var expect = require('chai').expect;
+
 var Hijackdi = require('../lib/hijackdi.js');
 
 
@@ -15,4 +17,27 @@ test("When faked, Then fake is not the real implementation",function(done){
 		var fakeDependency = require('./fakeDependency.js');
 		fakeDependency();
 	});
+});
+
+test('When multiple items faked, Then all are all dependencies are faked',function(){
+	var hijackdiConfig = {
+		fakes: {
+			'./fakeDependency.js': function(){
+			},
+			'./otherFakeDependency.js': function(){
+			}
+		}
+	};
+	var hijackdi = new Hijackdi(hijackdiConfig);
+	hijackdi.sandbox(function(){
+		var fakeDependency = require('./fakeDependency.js').
+			otherFakeDependency = require('./otherFakeDependency.js');
+			expect(function(){
+				fakeDependency();
+				otherFakeDependency();
+			}).to.not.throw('Not stubbed/mocked');
+			
+
+	});
+
 });
